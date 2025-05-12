b8a95258cd3e10aa9ee4b6d074d0038ef436c185
raniyaya <pinik6586@gamil.com>
2025-05-12 10:52:32 +0900

    a

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-//@Controller + @ResponseBody
+//@Controller(@Component의 역할) + @ResponseBody
 @RestController
 @RequiredArgsConstructor
 //final 인 변수를 초기화하는 생성자를 자동으로 생성해주는 역할을 하는 롬복 어노테이션
